d809ad46f6b1a38fead003f216f63cd624e7589f
Andrey <andrey.kozhevnikov@devexpress.com>
2020-10-26 08:58:05 +0300

    remove mobile

--- VB/CalculatedPropertiesSolution.Mobile/DataService.svc.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-Imports DevExpress.ExpressApp.Mobile.Services
-
-Namespace CalculatedPropertiesSolution.Mobile
-    Public Class DataService
-        Inherits MobileDataService(Of CalculatedPropertiesSolutionMobileApplication)
-
-    End Class
-End Namespace
