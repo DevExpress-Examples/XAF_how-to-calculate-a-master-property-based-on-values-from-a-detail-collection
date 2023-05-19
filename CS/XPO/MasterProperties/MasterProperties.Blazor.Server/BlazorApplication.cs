858aa2d1bdd817c4d150cb63357029ae954ded93
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-19 16:04:30 +0400

    xpo

---
+++ CS/XPO/MasterProperties/MasterProperties.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using MasterProperties.Blazor.Server.Services;
+
+namespace MasterProperties.Blazor.Server;
+
+public class MasterPropertiesBlazorApplication : BlazorApplication {
+    public MasterPropertiesBlazorApplication() {
+        ApplicationName = "MasterProperties";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += MasterPropertiesBlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    private void MasterPropertiesBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
